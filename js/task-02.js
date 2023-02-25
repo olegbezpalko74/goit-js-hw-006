9b59b27d47af2f55d4972e213d3cc2c33a8d902a
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-02-25 16:32:40 +0200

    opopop

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ul = document.querySelector('#ingredients');
+
+const liElements = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+  
+});
+
+ul.append(...liElements);
+
