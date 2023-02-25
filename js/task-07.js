306a9d5e38e638de07d2cce515d944e2908be65b
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-02-25 16:50:08 +0200

    update task #7

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,14 @@
-const fontSizeControl = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
+// const fontSizeControl = document.querySelector('#font-size-control');
+// const text = document.querySelector('#text');
 
-fontSizeControl.addEventListener('input', (event) => {
-  const fontSize = event.target.value;
-  text.style.fontSize = `${fontSize}px`;
-});   
+// fontSizeControl.addEventListener('input', (event) => {
+//   const fontSize = event.target.value;
+//   text.style.fontSize = `${fontSize}px`;
+// });   
+
+const inputEl = document.getElementById('font-size-control');
+const textEl = document.getElementById('text');
+
+inputEl.oninput = () => {
+  textEl.style.fontSize = inputEl.value + 'px';
+};
