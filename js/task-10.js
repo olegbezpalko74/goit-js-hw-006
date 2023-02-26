35bf44023699294f13439b4080a2adb062e93470
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-02-26 20:45:06 +0200

    up

--- js/task-10.js
+++ js/task-10.js
@@ -1,7 +1,40 @@
+
+
+const inputEl = document.querySelector('#controls input');
+const createBtn = document.querySelector('#controls [data-create]');
+const destroyBtn = document.querySelector('#controls [data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
+// функція для створення колекції елементів
+function createBoxes(amount) {
+  let boxSize = 30;
+  const boxes = [];
 
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    boxSize += 10;
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+// функція для очищення колекції елементів
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(inputEl.value);
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
